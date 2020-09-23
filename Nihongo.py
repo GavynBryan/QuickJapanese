82243b0074a7810301cd4c8923ae1cc6bfd81ca0
Gavyn Bryan <42544471+GavynBryan@users.noreply.github.com>
2020-09-22 22:29:09 -0400

    Update Nihongo.py
    Did some tidying up. Also allowed the script to run without any parameters, letting the user freely type in as many words as they want to until they type "!quit"

--- Nihongo.py
+++ Nihongo.py
@@ -5,13 +5,8 @@ import urllib.request, json
 from urllib.parse import quote
 
 
-English = sys.argv[len(sys.argv) - 1]
-English = quote(English)
-
-words = []
-with urllib.request.urlopen("https://jisho.org/api/v1/search/words?keyword=" + English) as url:
-    data = json.loads(url.read().decode())
-    words = data["data"]
+app_parameter = sys.argv[len(sys.argv) - 1]
+api_url = "https://jisho.org/api/v1/search/words?keyword="
 
 
 # converts a bool into yes or no
@@ -22,19 +17,36 @@ def y_or_n(sbool):
         return "No"
 
 
-for word in words:
-    # this is just how Jisho has their JSON organized. No bully, plz
-    japanese = word["japanese"][0]
-    senses = word["senses"][0]
+def lookup(phrase):
+    phrase = quote(phrase)
+    words = []
+    with urllib.request.urlopen(api_url + phrase) as url:
+        data = json.loads(url.read().decode())
+        words = data["data"]
 
-    slug = word.get("slug")
-    reading = japanese.get("reading")
-    definition = senses.get("english_definitions")
+    for word in words:
+        # this is just how Jisho has their JSON organized. No bully, plz
+        japanese = word["japanese"][0]
+        senses = word["senses"][0]
 
-    # only show valid results
-    if slug and reading:
-        print(f"{slug}  ({reading}) - {definition}")
-        if word.get("is_common"):
-            print("Common Word?: ", y_or_n(word["is_common"]))
+        slug = word.get("slug")
+        reading = japanese.get("reading")
+        definition = senses.get("english_definitions")
+
+        # only show valid results
+        if slug and reading:
+            print(f"{slug}  ({reading}) - {definition}")
+            if word.get("is_common"):
+                print("Common Word?: ", y_or_n(word["is_common"]))
 
 
+if app_parameter == sys.argv[0]:
+    finished = False
+    while not finished:
+        m_input = input("\nEnter word or type !quit to cancel: ")
+        if m_input != "!quit":
+            lookup(m_input)
+        else:
+            finished = True
+else:
+    lookup(app_parameter)
